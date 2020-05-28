3e6054eaa4e99625e74dbbcdd8e73862de775bc6
M Yousaf <helpdesk@fintaxconsultants.com.pk>
2020-05-29 00:59:32 +0500

    Admin panel update

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -38,7 +38,7 @@
         <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
       </li>
       <li class="nav-item d-none d-sm-inline-block">
-        <a href="index3.html" class="nav-link">Home</a>
+        <a href={{asset("/adminlte/index3.html")}} class="nav-link">Home</a>
       </li>
       <li class="nav-item d-none d-sm-inline-block">
         <a href="#" class="nav-link">Contact</a>
@@ -209,7 +209,7 @@
             </ul>
           </li>
           <li class="nav-item">
-            <a href="pages/widgets.html" class="nav-link">
+            <a href={{asset("pages/widgets.html")}} class="nav-link">
               <i class="nav-icon fas fa-th"></i>
               <p>
                 Widgets
