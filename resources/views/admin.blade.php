61c333be44680aeb8481d0f54cd7d0b2e475c8e4
M Yousaf <helpdesk@fintaxconsultants.com.pk>
2020-06-08 16:13:15 +0500

    admin

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -189,7 +189,7 @@
             </a>
             <ul class="nav nav-treeview">
               <li class="nav-item">
-                <a href="./index.html" class="nav-link active">
+                <a href="./adminlte/index.html" class="nav-link active">
                   <i class="far fa-circle nav-icon"></i>
                   <p>Dashboard v1</p>
                 </a>
