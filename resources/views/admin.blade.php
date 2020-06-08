8f37deb4b8982becec61763579e3642d720021ea
M Yousaf <helpdesk@fintaxconsultants.com.pk>
2020-06-08 16:06:00 +0500

    Dashboard hide

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -194,7 +194,7 @@
                   <p>Dashboard v1</p>
                 </a>
               </li>
-              <li class="nav-item">
+              {{-- <li class="nav-item">
                 <a href="./index2.html" class="nav-link">
                   <i class="far fa-circle nav-icon"></i>
                   <p>Dashboard v2</p>
@@ -205,7 +205,7 @@
                   <i class="far fa-circle nav-icon"></i>
                   <p>Dashboard v3</p>
                 </a>
-              </li>
+              </li> --}}
             </ul>
           </li>
           <li class="nav-item">
