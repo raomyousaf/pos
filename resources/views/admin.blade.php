7c36ef4ac94243e7d97f5b0a0ecdcaf1010d946e
M Yousaf <helpdesk@fintaxconsultants.com.pk>
2020-06-08 16:08:25 +0500

    Dashboard name to Home

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -183,7 +183,7 @@
             <a href="#" class="nav-link active">
               <i class="nav-icon fas fa-tachometer-alt"></i>
               <p>
-                Dashboard
+                Home
                 <i class="right fas fa-angle-left"></i>
               </p>
             </a>
